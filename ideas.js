85498da2e52e2c50cc8f14d4c4c8063fc5939dd4
Student <example@example.com>
2019-06-14 10:15:15 -0700

    added brainstorming for next idea

---
+++ ideas.js
@@ -0,0 +1,40 @@
+/*
+
+var power;
+var heat;
+var sMult;
+
+var maxHeat;
+var startingPower;
+
+
+the machine will go to the number that you give it, and will
+use that amount of power
+
+it will add heat whenenver it gets to a 3
+
+it will lose power for every second, beeps and boops take a bit longer
+
+the point is to get it to go to infinite power and speed, which occurs when the amount of speed that it is gaining from the overheat suprasses the amount of power it drains
+
+if it ever loses power completely it resets, and its heat goes to 0
+
+it gains less heat for every subsequent sorry after another sorry
+
+how to structure the entire thing:
+
+in a while loop: while !playerWon() has not been reached
+attach submit here:
+read: enter the next number
+then when submit is called,
+  starts for loop for number given.
+  timetaken is an inverse relation to heat
+
+beep and boop make actual sounds. so does im sorry dave
+
+beep and boop just make it subtract 3 times the normal power, but still in relation to the maxHeat
+
+function playerWon(){
+}
+
+*/
