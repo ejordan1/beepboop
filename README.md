b1d06fff8593d19fbfd584d4a63d33e9778b5647
emerz210 <emerz210@gmail.com>
2019-06-16 21:43:20 -0700

    add refactored code and timing alterations

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # _BeepBoop_
 
-#### _{Brief description of application}, {Date of current version}_
+#### _BeepBoop is a silly short game about Dave and a uncooperative computer, June 16, 2019_
 
-#### By _**{List of contributors}**_
+#### By _**Emerson Jordan**_
 
 ## Description
 
-_{This is a detailed description of your application. Its purpose and usage.  Give as much detail as needed to explain what the application does, and any other information you want users or other developers to have. }_
+_This is a program that takes in an input and displays number leading up to the input according to a specific set of rules. This premise was converted to simple game. For the full experience input any number above 500_
 
 _Specs:_
 _The program returns a range of numbers from 0 to the users inputted number_
@@ -25,28 +25,24 @@ _If there is a 1 contained in the number but neither a 3 or a 2, the program sho
 
 ## Setup/Installation Requirements
 
-* _This is a great place_
-* _to list setup instructions_
-* _in a simple_
-* _easy-to-understand_
-* _format_
+* _download the zip file from the github repository_
+* _unzip the file and open "index.html"_
 
-_{Leave nothing to chance! You want it to be easy for potential users, employers and collaborators to run your app. Do I need to run a server? How should I set up my databases? Is there other code this app depends on?}_
 
 ## Known Bugs
 
-_{Are there issues that have not yet been resolved that you want to let users know you know?  Outline any issues that would impact use of your application.  Share any workarounds that are in place. }_
+_There are no known bugs_
 
 ## Support and contact details
 
-_{Let people know what to do if they run into any issues or have questions, ideas or concerns.  Encourage them to contact you or make a contribution to the code.}_
+_If you find any bugs or have any issues contact me at emerz210@gmail.com_
 
 ## Technologies Used
 
-_{Tell me about the languages and tools you used to create this app. Assume that I know you probably used HTML and CSS. If you did something really cool using only HTML, point that out.}_
+_Javascript, browser_
 
 ### License
 
-*{Determine the license under which this application can be used.  See below for more details on licensing.}*
+*All rights reserved*
 
-Copyright (c) 2016 **_{List of contributors or company name}_**
+Copyright (c) 2019 **_Emerson Jordan_**
--- js/scripts3.js
+++ js/scripts3.js
@@ -10,9 +10,7 @@ $(function(){
 });
 
 var power = 0;
-var heat = 0;
 var sMult;
-var maxHeat = 100;
 var startingPower = 35;
 var stillComputing = false;
 var totalTries = 0;
@@ -21,28 +19,10 @@ var timesAnimPlays = 6;
 var regularSorryText = "I'm sorry, Dave. I'm afraid I can't do that";
 var sorrySplit;
 var sorryAdds = ["I", "m", "D", "v", "n't", "aht", "airfd", "Dav!", "%%", "&!&", "ERRR#)", "@*()", "orsy"]
-var computerBreakMargin = 60;
+var computerBreakMargin = 310;
 var delayMultiplier = 1.5;
 
-function compute(userString){
-  $("#computerPower").text(power);
-  $("#computerHeat").text(heat);
-  var input = parseInt(userString);
-  var valuesStored = [];
-  for (var i = 0; i < input + 1; i++){
-    if (containsStr("3", i)){
-      valuesStored.push("I'm sorry, Dave. I'm afraid I can't do that");
-    } else if(containsStr("2", i)){
-      valuesStored.push("Boop");
-    } else if(containsStr("1", i)){
-      valuesStored.push("Beep");
-    } else {
-      valuesStored.push(i);
-    }
-  }
-  return valuesStored.join(", ");
-}
-
+//this method takes in a string and a string, and returns true if the second string is contained in the first
 function containsStr(str, num){
   var numStr = num.toString();
   for(var i = 0; i < numStr.length; i++){
@@ -53,10 +33,10 @@ function containsStr(str, num){
   return false;
 }
 
-
+//This method starts the game and runs the timeCompute function and resets variables
 function computerOperate(userInput){
   totalTries++;
-   sorrySplit = regularSorryText.split("");
+  sorrySplit = regularSorryText.split("");
   if (!stillComputing){
     power = startingPower;
     stillComputing = true;
@@ -66,23 +46,22 @@ function computerOperate(userInput){
   }
 }
 
+//this is a recurssive function that takes in an input and runs until 398 (which is the player win condition). It replaces numbers according to a series of rules.
 function timeCompute(input, i, willWaitTime){
   setTimeout(function(){
-    if (i > 400){
+    if (i > 398){
       playerWins();
       return;
     }
-    var currentHeatTime = getTimeFromHeat(heat,maxHeat,i);
+    var currentHeatTime = getTimeFromHeat(i);
     updatePower();
     var incomingNums = getNumberStr(input, i, "");
     incomingAnim(incomingNums, 0, currentHeatTime / timesAnimPlays);
-    if (heat < computerBreakMargin){
+    if (i < computerBreakMargin){
       if (containsStr("3", i)){
         setTimeout(function(){
           $("#computerDisplay").text(regularSorryText);
-          heat++;
         }, currentHeatTime * delayMultiplier);
-        updateHeat();
       } else if(containsStr("2", i)){
         setTimeout(function(){
           $("#computerDisplay").text("boop");
@@ -97,54 +76,45 @@ function timeCompute(input, i, willWaitTime){
         }, currentHeatTime  * delayMultiplier);
       }
     } else {
-      heat++;
-      $("#computerDisplay").text(getSorryText(i));
-    }
-    // power -= currentHeatTime / 1000;
-    // if (playerWon()){
-    //   playerWins();
-    //   // console.log("player won!");
-    //   // timeCompute(input, i + 1); //keeps going forever, will crash users browser :)
-    // } else
+      setTimeout(function(){
+        $("#computerDisplay").text(getSorryText(i));
+      }, currentHeatTime  * delayMultiplier);
+      }
 
-    if (i < input){
-      //cannot run out of power
-      if (power > -1000){
+      if (i < input){
+        if (power > -1000){
         timeCompute(input, i + 1, currentHeatTime);
       } else {
-        //ran out of power
         ranOutOfPower();
       }
     } else {
       inputRanOut();
     }
-    //clears screen before next one
     setTimeout(function(){
       $("#computerDisplay").text("");
     }, (currentHeatTime  * 1.5) + currentHeatTime - 10);
   }, willWaitTime);
 }
 
-
-
-function getTimeFromHeat(inHeat, inMaxHeat, counter){
-  if (counter < 2){
+//takes in counter and returns the speed the numbers will display at
+function getTimeFromHeat(counter){
+  if (counter < 6){
+    return 1200;
+  }
+  if (counter < 7){
     return 800;
   }
-  if (counter < 6){
-    return 500;
+  if (counter < 16){
+    return 600;
   }
-  if (counter < 12){
-    return 300;
+  if (counter < 30){
+    return 250;
   }
-  if (counter < 20){
-    return 150;
-  }
-  if (counter < 40){
-    return 120;
+  if (counter < 50){
+    return 140;
   }
   if (counter < 80){
-    return 95;
+    return 120;
   }
   if (counter < 100){
     return 80;
@@ -156,18 +126,10 @@ function getTimeFromHeat(inHeat, inMaxHeat, counter){
     return 50;
   }
   return 40;
-  // var time = Math.pow(0.05825 * inHeat, 2) + (6.479 * inHeat) - 5;
-  // return time;
-}
-
-function playerWon(){
-  // console.log (heat + " is heat, and max: " + maxHeat);
-  return heat > maxHeat;
 }
 
 function ranOutOfPower(){
   stillComputing = false;
-  heat = 0;
   power = 0;
   updatePower();
   updateHeat();
@@ -177,10 +139,9 @@ function ranOutOfPower(){
   }, 50);
 }
 
+//displays text when the number is reached but the player has not won
 function inputRanOut(){
   stillComputing = false;
-  heat = heat / 1.5;
-
   setTimeout(function(){
     $("#computerDisplay").text("Maybe next time, Dave. Maybe next time.");
   }, 2500);
@@ -201,22 +162,13 @@ function inputRanOut(){
   }, 1000);
 }
 
-function updateHeat(){
-  $("#computerHeat").text("Internal temperature:    " + Math.ceil(heat));
-}
-
 function updatePower(){
   $("#computerPower").text("Power left:    " + Math.ceil(power));
 }
 
+//displays text when player wins
 function playerWins(){
-
   power = 0;
-  heat = 0;
-  var threes = "";
-  for (var i = 0; i < 100; i++){
-    threes += "3";
-  }
   setTimeout(function(){
     $("#incomingNumbersSpan").text("");
   }, 50);
@@ -239,18 +191,18 @@ function playerWins(){
   }, 11100);
 }
 
-
+//simulates the animation of the incoming numbers
 function incomingAnim(str, i, heatTime){
   setTimeout(function(){
     if (i > timesAnimPlays){
       return;
     }
-    //sets new string to substring of i
     $("#incomingNumbersSpan").text(str.substring(i, i + incomingTotalLength));
     incomingAnim(str, i + 1, heatTime);
-    //repeats at duratin of interval / times it will switch between new numbers
   }, heatTime);
 }
+
+//takes in a max index, currentindex, and current str, and returns the current index plus the previous string with punctuation additions
 function getNumberStr(maxNum, currentIndex, str){
   if (currentIndex > maxNum){
     return str;
@@ -261,30 +213,27 @@ function getNumberStr(maxNum, currentIndex, str){
   return getNumberStr(maxNum, currentIndex + 1, newStr);
 }
 
+//takes in the current index of the count, and returns the "Sorry Dave" text
 function getSorryText(counter){
-  if  (heat < computerBreakMargin){
+  if  (counter < 300){
     return regularSorryText;
   } else {
-    // var ran = Math.random();
-    // console.log(ran);
-    if (counter > 300){
-      if (counter < 310){
-        if (Math.random() < .1){
-          sorrySplit[Math.floor(Math.random() * sorrySplit.length)] += sorryAdds[Math.floor(Math.random() * (sorryAdds.length - 1))];
-        }
-      } else if (counter < 350){
-        if (Math.random() < .16){
-          sorrySplit[Math.floor(Math.random() * sorrySplit.length)] += sorryAdds[Math.floor(Math.random() * (sorryAdds.length - 1))];
-        }
-      } else if (counter < 380){
-        if (Math.random() < .5){
-          sorrySplit[Math.floor(Math.random() * sorrySplit.length)] += sorryAdds[Math.floor(Math.random() * (sorryAdds.length - 1))];
-        }
-      } else {
+    debugger;
+    if (counter < 310){
+      if (Math.random() < .1){
         sorrySplit[Math.floor(Math.random() * sorrySplit.length)] += sorryAdds[Math.floor(Math.random() * (sorryAdds.length - 1))];
       }
+    } else if (counter < 350){
+      if (Math.random() < .16){
+        sorrySplit[Math.floor(Math.random() * sorrySplit.length)] += sorryAdds[Math.floor(Math.random() * (sorryAdds.length - 1))];
+      }
+    } else if (counter < 380){
+      if (Math.random() < .5){
+        sorrySplit[Math.floor(Math.random() * sorrySplit.length)] += sorryAdds[Math.floor(Math.random() * (sorryAdds.length - 1))];
+      }
+    } else {
+      sorrySplit[Math.floor(Math.random() * sorrySplit.length)] += sorryAdds[Math.floor(Math.random() * (sorryAdds.length - 1))];
     }
-    console.log(sorrySplit.join(""));
     str = sorrySplit.join("");
     if (str.length > 100){
       str = str.substring(str.length - 90, str.length);
